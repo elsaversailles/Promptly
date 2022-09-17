344b32311a2cf3e026b59554491fc8d5b2ac03b9
Vince Austria <50725388+elsaversailles@users.noreply.github.com>
2022-09-17 14:34:49 +0800

    PHP handler for the mainpage

---
+++ prompt_result.php
@@ -0,0 +1,33 @@
+<html>
+<body>
+<h2>Prompt Result page </h2> <br>
+<br> Personal Information <br> <br>
+First name: <?php echo $_POST["primary"]; ?><br>
+Middle initial: <?php echo $_POST["secondary_subject"]; ?> <br>
+Last name: <?php echo $_POST["weather_selector"]; ?> <br>
+Bday: <?php echo $_POST["datepicker"]; ?> <br>
+Gender: <?php echo $_POST["gender_selector"]; ?> <br>
+<br>
+Skills
+<br>
+Skill 1: <?php echo $_POST["skill_1"]; ?> <br> 
+Skill 2: <?php echo $_POST["skill_2"]; ?> <br>
+Skill 3: <?php echo $_POST["skill_3"]; ?> <br>
+Skill 4: <?php echo $_POST["skill_4"]; ?> <br>
+<br>
+Bio: <?php echo $_POST["bio"]; ?> <br>
+<br>
+Educational profile <br> <br>
+Current school: <?php echo $_POST["current_school"]; ?> <br>
+Current Educational Attainment: <?php echo $_POST["cur_educ_at"]; ?> <br>
+Course/Strand: <?php echo $_POST["cor_str"]; ?> <br>
+Specialization: <?php echo $_POST["specialization"]; ?> <br>
+Learning style: <?php echo $_POST["ls"]; ?> <br>
+Phone number: <?php echo $_POST["phone_num"]; ?> <br>
+<br>
+Work Profile
+<br> <br>
+Current work: <?php echo $_POST["current_work"]; ?> <br>
+
+</body>
+</html> 
